48c04efdc243af668ed3fc8f7abb173c288d980e
MZYugu <manami.u.yugu@gmail.com>
2023-05-29 23:04:03 +0900

    exampleの整理

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "nickname,email,password,password_confirmation,lastname_kanji,firstname_kanji,lastname_katakana,firstname_katakana,birthdayが正しく存在すれば登録できる" do
+    end
+    it "nicknameが空だと登録できない" do
+    end
+    it "emailが空だと登録できない" do
+    end
+    it "重複したemailが存在すると登録できない" do
+    end
+    it "emailに@が含まれていないと登録できない" do
+    end
+    it "passwordが空だと登録できない" do
+    end
+    it "passwordが5文字以下だと登録できない" do
+    end
+    it "passwordが英数混合でないと登録できない" do
+    end
+    it "passwordとpassword_confirmationが一致していないと登録できない" do
+    end
+    it "lastname_kanjiが空だと登録できない" do
+    end
+    it "lastname_kanjiが全角（漢字・ひらがな・カタカナ）でないと登録できない" do
+    end
+    it "firstname_kanjiが空だと登録できない" do
+    end
+    it "firstname_kanjiが全角（漢字・ひらがな・カタカナ）でないと登録できない" do
+    end
+    it "lastname_katakanaが空だと登録できない" do
+    end
+    it "lastname_katakanaが全角（カタカナ）でないと登録できない" do
+    end
+    it "firstname_katakanaが空だと登録できない" do
+    end
+    it "firstname_katakanaが全角（カタカナ）でないと登録できない" do
+    end
+    it "birthdayが空だと登録できない" do
+    end
+    it "" do
+    end
+  end
+end
