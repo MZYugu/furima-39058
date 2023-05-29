fcf1204e4dfd0d05fc840d8b7b00208ba5cbc2fe
MZYugu <manami.u.yugu@gmail.com>
2023-05-30 01:10:04 +0900

    Fakerでテスト用の値を設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Internet.username}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    lastname_kanji
+    firstname_kanji
+    lastname_katakana
+    firstname_katakana
+    birthday              {Faker::Date.birthday(min_age: 10, max_age: 100)}
   end
 end
