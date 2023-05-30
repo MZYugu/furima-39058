f4f7c7de17e38c6a35945b3126cdceaefd955718
MZYugu <manami.u.yugu@gmail.com>
2023-05-30 19:32:31 +0900

    FactoryBotの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+
+    test_user = Gimei.name
+
     nickname              {Faker::Internet.username}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    lastname_kanji
-    firstname_kanji
-    lastname_katakana
-    firstname_katakana
+    lastname_kanji        {test_user.last.kanji}
+    firstname_kanji       {test_user.first.kanji}
+    lastname_katakana     {test_user.last.katakana}
+    firstname_katakana    {test_user.first.katakana}
     birthday              {Faker::Date.birthday(min_age: 10, max_age: 100)}
   end
 end
