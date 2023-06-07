79dd984e499b1ab042d02cd564fb89ef2d0884d5
MZYugu <manami.u.yugu@gmail.com>
2023-06-07 22:38:21 +0900

    画像ファイルの保存機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,6 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:item_name, :price, :text, :category_id, :item_status_id, :postage_bare_id, :prefecture_id, :days_ship_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :price, :text, :category_id, :item_status_id, :postage_bare_id, :prefecture_id, :days_ship_id, :image).merge(user_id: current_user.id)
   end
 end
