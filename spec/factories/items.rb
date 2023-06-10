bf2c9373eef410479fa33faf97debc936d9bab8e
MZYugu <manami.u.yugu@gmail.com>
2023-06-10 23:39:00 +0900

    itemのfactorybotの設定、exampleの整理

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    item_name       {Faker::Lorem.word}
+    price           {Faker::Number.between(from: 300, to: 9999999)}
+    text            {Faker::Lorem.paragraph}
+    category_id     {Faker::Number.between(from: 2, to: 11)}
+    item_status_id  {Faker::Number.between(from: 2, to: 7)}
+    postage_bare_id {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id   {Faker::Number.between(from: 2, to: 48)}
+    days_ship_id    {Faker::Number.between(from: 2, to: 4)}
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/IMG_4367.JPG'), filename: 'IMG_4367.JPG')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:itam)
+  end
+
+  describe '出品情報の保存' do
+    context '出品情報が保存できるとき' do
+      it 'item_name,price,text,category_id,item_status_is,postage_bare_id,prefecture_id,days_ship_id,user_idが正しく存在すれば保存できる' do
+      end
+    end
+
+    context '出品情報が保存できないとき' do
+      it 'item_nameが空だと出品できない' do
+      end
+
+      it 'priceが空だと出品できない' do
+      end
+
+      it 'priceが299以下だと出品できない' do
+      end
+
+      it 'priceが10,000,000以上だと出品できない' do
+      end
+
+      it 'textが空だと出品できない' do
+      end
+
+      it 'category_idが1だと出品できない' do
+      end
+
+      it 'item_status_idが1だと出品できない' do
+      end
+
+      it 'postage_bare_idが1だと出品できない' do
+      end
+
+      it 'prefecture_idが1だと出品できない' do
+      end
+      
+      it 'days_ship_idが1だと出品できない' do
+      end
+
+      it 'ユーザーが紐づいていなければ出品できない' do
+      end
+      end
+    end
+  end
 end
