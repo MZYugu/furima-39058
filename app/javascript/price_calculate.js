9d2a81bbe4822f78cc8abe223c4893c4ac775be3
MZYugu <manami.u.yugu@gmail.com>
2023-06-11 21:41:50 +0900

    販売手数料と利益の表示

--- app/javascript/price_calculate.js
+++ app/javascript/price_calculate.js
@@ -1,2 +1,11 @@
 window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const valueInput = priceInput.value;
+    const priceTax = document.getElementById("add-tax-price");
+    const calcTax = Math.floor(valueInput * 0.1);
+    priceTax.innerHTML = calcTax;
+    const profit = document.getElementById("profit");
+    profit.innerHTML = valueInput - calcTax
+  })
 })
