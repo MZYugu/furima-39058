0f7aac5d0e7a09333013bec637f002c5ebd19aff
MZYugu <manami.u.yugu@gmail.com>
2023-06-18 15:03:35 +0900

    コントローラーにshowアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
   
+  def show
+    @item = Item.find(params[:id])
+  end
+  
   private
   def item_params
     params.require(:item).permit(:item_name, :price, :text, :category_id, :item_status_id, :postage_bare_id, :prefecture_id, :days_ship_id, :image).merge(user_id: current_user.id)
