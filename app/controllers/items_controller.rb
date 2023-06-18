a00b1d4a1c2c57ec2b0a99af9e11d49e9154bb4c
MZYugu <manami.u.yugu@gmail.com>
2023-06-18 21:41:32 +0900

    editアクションupdateアクションの設定、ルーティグの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :get_item, only[:show, :edit]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @items = Item.all.order("created_at DESC")
     @count = Item.count
@@ -19,11 +21,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.includes(:user).find(params[:id])
+  end
+  
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
 
   private
   def item_params
     params.require(:item).permit(:item_name, :price, :text, :category_id, :item_status_id, :postage_bare_id, :prefecture_id, :days_ship_id, :image).merge(user_id: current_user.id)
+  end
+
+  def get_item
+    @item = Item.includes(:user).find(params[:id])
   end
 end
