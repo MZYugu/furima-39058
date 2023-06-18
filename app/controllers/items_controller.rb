eaed19cb98b581057d2ab51fdec9abe100767c08
MZYugu <manami.u.yugu@gmail.com>
2023-06-18 18:56:02 +0900

    ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,11 @@ class ItemsController < ApplicationController
     @items = Item.all.order("created_at DESC")
     @count = Item.count
   end
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -17,11 +17,11 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @item = Item.includes(:user).find(params[:id])
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:item_name, :price, :text, :category_id, :item_status_id, :postage_bare_id, :prefecture_id, :days_ship_id, :image).merge(user_id: current_user.id)
