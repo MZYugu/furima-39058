d26898a4a805ffb78786d57f2c1f4434a7a9357e
MZYugu <manami.u.yugu@gmail.com>
2023-06-18 21:52:47 +0900

    editビューファイルへの反映、コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :get_item, only[:show, :edit]
+  before_action :get_item, only: [:show, :edit]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
