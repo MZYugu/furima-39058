e90f7f7062fc1daf596374f93639d7683512a5c3
MZYugu <manami.u.yugu@gmail.com>
2023-06-18 22:35:43 +0900

    update失敗後のページの遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :get_item, only: [:show, :edit]
+  before_action :get_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -27,9 +27,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item.id)
+    if @item.update(item_params)
+      redirect_to item_path
     else
       render :edit
     end
