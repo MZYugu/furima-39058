0f866f439e9dd410a92d41b3fca0805f6f767b8c
MZYugu <manami.u.yugu@gmail.com>
2023-06-29 21:22:28 +0900

    マイグレーションファイルの修正

--- db/migrate/20230626132349_create_addresses.rb
+++ db/migrate/20230626132349_create_addresses.rb
@@ -6,8 +6,8 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :cities,        null: false
       t.string     :address,       null: false
       t.string     :building
-      t.string     :tel_number,     null: false
-      t.references :order,          null: false,foreign_key: true
+      t.string     :tel_number,    null: false
+      t.references :order,         null: false, foreign_key: true
 
       t.timestamps
     end
