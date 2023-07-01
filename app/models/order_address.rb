de7992c54e31c40bcd58f69664699387a7b1f224
MZYugu <manami.u.yugu@gmail.com>
2023-07-01 23:16:10 +0900

    order_addressモデルの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,20 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :cities, :address, :building, :tel_number, :order_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :cities, :address, :building, :tel_number, :user_id, :item_id
   
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :cities
     validates :address
     validates :tel_number, format: {with: /\A0[0-9]{9,10}\z/, message: "is invalid. Exclude hyphen(-)"}
-    validates :order_id
+    validates :user_id
+    validates :item_id
   end
 
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   
   def save
-    order = Order.create(order_id: order_id, user_id: user_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, cities: cities, address: address, building: building, tel_number: tel_number, order_id: order.id)
   end
 end
