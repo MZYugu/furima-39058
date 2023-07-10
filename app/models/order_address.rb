250bb06ed7d5fedda51416ac240b004016e201cc
MZYugu <manami.u.yugu@gmail.com>
2023-07-10 21:19:59 +0900

    電話番号のバリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :cities
     validates :address
-    validates :tel_number, format: {with: /\A0[0-9]{9,10}\z/, message: "is invalid. Exclude hyphen(-)"}
+    validates :tel_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Exclude hyphen(-)"}
     validates :user_id
     validates :item_id
     validates :token
